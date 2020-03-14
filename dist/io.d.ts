78dfc2c90aa0c5e41fbdf66f7bd398cc7aa5983c
Daryl Serrano Hipolito <darylserrano39@gmail.com>
2020-03-14 21:21:11 +0100

    Update dependencies (#3)
    * Bump @darylserrano/kindle-clippings package version and Patch version of this package
    
    * Deleted dist/ files and enhanced build script

--- dist/io.d.ts
+++
@@ -1,6 +0,0 @@
-import { KindleEntryParsed } from "@darylserrano/kindle-clippings";
-export declare function getAllEntriesParsed(filePath: string): Promise<Array<KindleEntryParsed>>;
-export declare function saveAll(dataToSave: Array<KindleEntryParsed>, pathToSave: string, filename?: string, pretty?: boolean): Promise<string>;
-export declare function saveByAuthor(dataToSave: Array<KindleEntryParsed>, pathToSave: string, pretty?: boolean): Promise<string>;
-export declare function saveByBookTitle(dataToSave: Array<KindleEntryParsed>, pathToSave: string, pretty?: boolean): Promise<string>;
-//# sourceMappingURL=io.d.ts.map
